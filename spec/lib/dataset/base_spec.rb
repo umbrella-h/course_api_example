66cc4e4e0f01fa7a085eb3b16f2a058cc82f4329
KasaHNO3 <umbrella.hsiao@gmail.com>
2023-07-11 20:06:49 +0800

    feat(lib, config): improve in-memory data manipulation.

--- spec/lib/dataset/base_spec.rb
+++ spec/lib/dataset/base_spec.rb
@@ -22,9 +22,9 @@ RSpec.describe Dataset::Base do
 
   context 'where' do
     it 'happy path' do
-      data = described_class.instance.where(table_name: 'users', query_hash: { name: 'Test User A'})
+      data = described_class.instance.where(table_name: 'users', query_hash: { name: 'Test User A' })
 
-      expect(data).to match_array([{ id: 123, name: 'Test User A', email: 'aa@t'}, { id: 456, name: 'Test User A', email: 'ab@t'}])
+      expect(data).to match_array([{ id: 123, name: 'Test User A', email: 'aa@t' }, { id: 456, name: 'Test User A', email: 'ab@t' }])
     end
   end
 
@@ -32,7 +32,7 @@ RSpec.describe Dataset::Base do
     it 'happy path' do
       data = described_class.instance.find(table_name: 'users', primary_hash: { id: 456 })
 
-      expect(data).to eq({ id: 456, name: 'Test User A', email: 'ab@t'})
+      expect(data).to eq({ id: 456, name: 'Test User A', email: 'ab@t' })
     end
   end
 
@@ -40,15 +40,15 @@ RSpec.describe Dataset::Base do
     it 'happy path' do
       described_class.instance.update(table_name: 'users', primary_hash: { id: 789 }, attributes: { name: 'Test User D' })
 
-      expect(described_class.instance.find(table_name: 'users', primary_hash: { id: 789 })).to eq({ id: 789, name: 'Test User D', email: 'c@t'})
+      expect(described_class.instance.find(table_name: 'users', primary_hash: { id: 789 })).to eq({ id: 789, name: 'Test User D', email: 'c@t' })
     end
   end
 
   context 'create' do
     it 'happy path' do
-      described_class.instance.create(table_name: 'users', primary_hash: { id: 012 }, attributes: { id: 012, name: 'Test User D', email: 'd@t' })
+      described_class.instance.create(table_name: 'users', primary_hash: { id: 012 }, attributes: { name: 'Test User D', email: 'd@t' })
 
-      expect(described_class.instance.find(table_name: 'users', primary_hash: { id: 012 })).to eq({ id: 012, name: 'Test User D', email: 'd@t'})
+      expect(described_class.instance.find(table_name: 'users', primary_hash: { id: 012 })).to eq({ id: 012, name: 'Test User D', email: 'd@t' })
     end
   end
 
@@ -56,7 +56,7 @@ RSpec.describe Dataset::Base do
     it 'happy path' do
       described_class.instance.delete(table_name: 'users', primary_hash: { id: 123 })
 
-      expect(described_class.instance.tables[:users]).to match_array([{ id: 789, name: 'Test User C', email: 'c@t'}, { id: 456, name: 'Test User A', email: 'ab@t'}])
+      expect(described_class.instance.tables[:users]).to match_array([{ id: 789, name: 'Test User C', email: 'c@t' }, { id: 456, name: 'Test User A', email: 'ab@t' }])
     end
   end
 
@@ -69,9 +69,9 @@ RSpec.describe Dataset::Base do
         { id: 456, name: 'Test Course B' },
       ],
       users: [
-        { id: 123, name: 'Test User A', email: 'aa@t'},
-        { id: 456, name: 'Test User A', email: 'ab@t'},
-        { id: 789, name: 'Test User C', email: 'c@t'},
+        { id: 123, name: 'Test User A', email: 'aa@t' },
+        { id: 456, name: 'Test User A', email: 'ab@t' },
+        { id: 789, name: 'Test User C', email: 'c@t' },
       ],
     }
   end
