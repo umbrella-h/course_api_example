617ae7b6d75b2b9ecce4fe57e96d423d9810ecf4
KasaHNO3 <umbrella.hsiao@gmail.com>
2023-07-12 07:03:49 +0800

    fix(app): fix bad request log status

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -11,7 +11,13 @@ module Api::V1
     end
 
     def bad_request(failure_message:)
-      render json: failed_json(code: 400, message: "Bad Request: #{failure_message}")
+      render json: failed_json(code: 400, message: "Bad Request: #{failure_message}"), status: :bad_request
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        ActiveSupport::SecurityUtils.secure_compare(token, ENV['ADMIN_AUTH_TOKEN'])
+      end
     end
   end
 end
